fd52c630b25dd5d0ffc364c868c81b10fa34616b
Ben <Ih8j200@gmail.com>
2024-07-29 18:27:04 +0100

    reply

--- commands/minecraftface.js
+++ commands/minecraftface.js
@@ -14,30 +14,8 @@ module.exports = {
         ],
     },
     async execute(interaction, client) {
-        const axios = require('axios');
         const username = interaction.options.getString('username');
         const url = `https://mineskin.eu/helm/${username}/100.png`;
-        await interaction.deferReply();
-        axios.get(url, {responseType: 'arraybuffer'}).then(response => {
-            const image = Buffer.from(response.data, 'binary').toString('base64');
-            const embed = {
-                "type": 4,
-                "data": {
-                    "content": "Here is the Minecraft face for " + username,
-                    "embeds": [
-                        {
-                            "title": username,
-                            "image": {
-                                "url": `data:image/png;base64,${image}`
-                            }
-                        }
-                    ]
-                }
-            };
-            client.api.interactions(interaction.id, interaction.token).callback.post({data: embed});
-        }).catch(error => {
-            console.error(error);
-            interaction.editReply('Error getting Minecraft face.');
-        });
+        await interaction.reply({content: url});
     },
 }
